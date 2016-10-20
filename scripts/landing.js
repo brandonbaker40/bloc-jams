624c726c2499d13ea608594d75732bfc5fb05701
Brandon Baker <BrandonBaker@Brandons-MacBook-Air.local>
2016-10-20 12:45:40 -0400

    checkpoint 10 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,10 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function(index) {
-    points[index].style.opacity= 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+var revealPoint = function(point) {
+    point.style.opacity= 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
 	
 var animatePoints = function(points) {		
@@ -13,15 +13,16 @@ var animatePoints = function(points) {
 
 window.onload = function() {
     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+        animatePoints(pointsArray);
      }
+    
     
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
     
     window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        if (document.body.scrollTop >= scrollDistance) {
              animatePoints(pointsArray);   
          }
      });
