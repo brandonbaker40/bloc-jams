c6edba3d61c89283fc76e39f4191927e7eef2f73
Brandon Baker <BrandonBaker@Brandons-MacBook-Air.local>
2016-10-31 14:56:58 -0400

    checkpoint 19 part 1 of assignment done

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso', 
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21' },
+        { title: 'Magenta', duration: '2:15' }
+    ]
+};
+
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
+
+var albumMLBBiggestHits = {
+     title: 'The Sultan of Swat',
+     artist: 'Babe Ruth',
+     label: 'Major League Records',
+     year: '1921',
+     albumArtUrl: 'assets/images/album_covers/18.png',
+     songs: [
+         { title: 'King of Crash', duration: '2:22' },
+         { title: 'Better Than Barry Bonds', duration: '3:13' },
+         { title: 'Never Needed A Mitt Anyway', duration: '2:45'},
+         { title: 'We Gotta Beat Boston', duration: '4:54' },
+         { title: 'Mic Drop', duration: '0:15'}
+     ]
+ };
