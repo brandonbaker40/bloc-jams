877dfa7e1cb308105a91d927a7d7d933cc8c57f4
Brandon Baker <BrandonBaker@Brandons-MacBook-Air.local>
2016-10-31 23:04:12 -0400

    checkpoint 20 checkpoint work done....realizing I have to go thru and find the previous song variable before it works correctly

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,21 @@
 var setSong = function (songNumber) {
-	currentlyPlayingSongNumber = parseInt(songNumber);
+	if (currentSoundFile) {
+         currentSoundFile.stop();
+     }
+    currentlyPlayingSongNumber = parseInt(songNumber);
 	currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+       formats: [ 'mp3'],
+       preload: true
+    });
+    
+    setVolume(currentVolume);
+};
+
+var setVolume = function(volume) {
+  if (currentSoundFile) {
+      currentSoundFile.setVolume(volume);
+  }  
 };
 
 var getSongNumberCell = function (number) {
@@ -54,12 +69,18 @@ var clickHandler = function () {
         if (currentlyPlayingSongNumber !== songNumber) {
             $(this).html(pauseButtonTemplate);
             setSong(songNumber);
+            currentSoundFile.play();
         } else if (currentlyPlayingSongNumber === songNumber) {
-            $(this).html(playButtonTemplate);
-            $('.main-controls .play-pause').html(playerBarPlayButton);
-            currentlyPlayingSongNumber = null;
-            currentSongFromAlbum;
+            if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplateButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
         }
+    }
 };
 
 
@@ -126,6 +147,8 @@ var playerBarPauseButton = '<span class="ion-pause"></span>'
  var currentAlbum = null;
  var currentlyPlayingSongNumber = null;
  var currentSongFromAlbum = null;
+ var currentSoundFile = null;
+ var currentVolume = 80;
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
 
@@ -136,6 +159,8 @@ $(document).ready(function() {
 
 });
 
+
+//I AM MISSING THIS! var previousSong.....what checkpoint is this from?
 
 var nextSong = function () {
   
@@ -151,6 +176,7 @@ var nextSong = function () {
     }
     
     currentlyPlayingSongNumber = currentSongIndex + 1;
+    currentSoundFile.play();
     setSong(songNumber);
     
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -5,11 +5,11 @@ var albumPicasso = {
     year: '1881',
     albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21' },
-        { title: 'Magenta', duration: '2:15' }
+        { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue'},
+        { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green'},
+        { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red'},
+        { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink'},
+        { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta'}
     ]
 };
 
@@ -20,11 +20,11 @@ var albumMarconi = {
      year: '1909',
      albumArtUrl: 'assets/images/album_covers/20.png',
      songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21' },
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15' }
+         { title: 'Hello, Operator?', duration: '1:01', audioUrl: 'assets/music/blue'},
+         { title: 'Ring, ring, ring', duration: '5:01', audioUrl: 'assets/music/green'},
+         { title: 'Fits in your pocket', duration: '3:21', audioUrl: 'assets/music/red'},
+         { title: 'Can you hear me now?', duration: '3:14', audioUrl: 'assets/music/pink'},
+         { title: 'Wrong phone number', duration: '2:15', audioUrl: 'assets/music/magenta'}
      ]
  };
 
@@ -35,10 +35,10 @@ var albumMLBBiggestHits = {
      year: '1921',
      albumArtUrl: 'assets/images/album_covers/18.png',
      songs: [
-         { title: 'King of Crash', duration: '2:22' },
-         { title: 'Better Than Barry Bonds', duration: '3:13' },
-         { title: 'Never Needed A Mitt Anyway', duration: '2:45'},
-         { title: 'We Gotta Beat Boston', duration: '4:54' },
-         { title: 'Mic Drop', duration: '0:15'}
+         { title: 'King of Crash', duration: '2:22', audioUrl: 'assets/music/blue'},
+         { title: 'Better Than Barry Bonds', duration: '3:13', audioUrl: 'assets/music/green'},
+         { title: 'Never Needed A Mitt Anyway', duration: '2:45', audioUrl: 'assets/music/red'},
+         { title: 'We Gotta Beat Boston', duration: '4:54', audioUrl: 'assets/music/pink'},
+         { title: 'Mic Drop', duration: '0:15', audioUrl: 'assets/music/magenta'}
      ]
  };
